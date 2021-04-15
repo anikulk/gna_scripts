4a7fbd1f0a146cf59c6f2714fded63a62b2a127c
Anisha Kulkarni <anisha.dattatraya.kulkarni@intel.com>
2021-04-15 15:18:47 -0700

    Redense models profiling

--- check_op.py
+++ check_op.py
@@ -18,10 +18,10 @@ class LabelHTMLParser(HTMLParser):
 
     def get_identity_layer_map(self):
         return self.identity_layer_map
-    
+
     def get_affine_layer_map(self):
         return self.affine_layer_map
-    
+
     def get_identity_wscale_map(self):
         return self.identity_wscale_map
 
@@ -36,9 +36,13 @@ class LabelHTMLParser(HTMLParser):
         for item in self.list:
             if("identity" in item[5]):
                 curr_layer = gna_layers.IdentityLayer(item[5], item[2], item[29])
+                print("item2 = " + str(item[2]))
                 layer_no = item[2].split("_")
+                print("identity layer" + str(curr_layer))
                 self.identity_layer_map[int(layer_no[1])] = curr_layer
+                print("identity layer number" + str(layer_no[1]))
                 self.identity_wscale_map[int(layer_no[1])] = item[29]
+                print("identity layer wscale" + str(item[29]))
             if("kDnnAffineOp" in item[2]):
                 curr_layer = gna_layers.AffineLayer(item[5], item[2], item[44], item[11])
                 layer_no = item[2].split("_")
@@ -59,7 +63,7 @@ def convert_and_write_to_csv(frame_start, frame_end, filelist, csvname):
                     output_list = []
                     for data in f:
                         #order = data.replace("\n", ",").replace('\n','').strip()
-                        output = data.split("\n") # Can just split by \n? 
+                        output = data.split("\n") # Can just split by \n?
                         output_list.append(float(output[0].strip()))
                     layerwriter.writerow(output_list)
 
@@ -71,7 +75,7 @@ def parse_dot(dot_file_path):
     for item in P_list[0].obj_dict['nodes'].keys():
         label_parser.feed(P_list[0].obj_dict['nodes'][item][0]['attributes']['label'])
         label_parser.add_data_list()
-    
+
     label_parser.create_layer()
     return label_parser
 
@@ -93,8 +97,8 @@ def main():
             os.mkdir(str(frame))
             os.mkdir(str(frame) + "/identity")
             os.mkdir(str(frame) + "/identityfp32")
-            subprocess.run(["scp", "root@" + dut_ip + ":/tmp/layers/" + str(frame) + "/*-kActIdentity_output.txt", "./" + str(frame) + "/identity/"])
-            subprocess.run(["scp", "root@" + dut_ip + ":/tmp/ref_layers/" + str(frame) + "/*-kActIdentity_output.txt", "./" + str(frame) + "/identityfp32"])
+            subprocess.run(["scp", "root@" + dut_ip + ":/usr/local/ml_benchmark/layers/" + str(frame) + "/*-kActIdentity_output.txt", "./" + str(frame) + "/identity/"])
+            subprocess.run(["scp", "root@" + dut_ip + ":/usr/local/ml_benchmark/ref_layers/" + str(frame) + "/*-kActIdentity_output.txt", "./" + str(frame) + "/identityfp32"])
     #convert_and_write_to_csv(0, 3, [42, 66, 54, 30, 122, 143, 186, 164, 217, 238, 259, 281, 312, 333, 354, 376, 407, 428, 449, 470, 502, 523, 544, 566], "ln_ip.csv")
     if parse_graph == 'true':
         graph = parse_dot(dot_file_path)
@@ -110,28 +114,28 @@ def main():
             layers_op = []
             fp32_layers_op = []
             file_count = 0
-            for filename in os.listdir(os.getcwd() + "/" + str(frame) + "/identityfp32/0"):
+            for filename in os.listdir(os.getcwd() + "/" + str(frame) + "/identityfp32/"):
                 layers_op.append([])
                 fp32_layers_op.append([])
                 fp32_layers_op[file_count].append(filename)
                 layers_op[file_count].append(filename)
 
-                with open(os.getcwd() + "/" + str(frame) + "/identity/0/" + str(filename), 'r') as f:
+                with open(os.getcwd() + "/" + str(frame) + "/identity/" + str(filename), 'r') as f:
                     layer_op = []
                     output_list = []
 
                     for data in f:
                         order = data.replace("\n", ",").replace('\n','').strip()
-                        output = order.split(",") # Can just split by \n? 
+                        output = order.split(",") # Can just split by \n?
                         output_list.append(output[0])
 
-                with open(os.getcwd() + "/" + str(frame) + "/identityfp32/0/" + str(filename), 'r') as f:
+                with open(os.getcwd() + "/" + str(frame) + "/identityfp32/" + str(filename), 'r') as f:
                     fp32_layer_op = []
                     fp32_output_list = []
 
                     for data in f:
                         order = data.replace("\n", ",").replace('\n','').strip()
-                        output = order.split(",") # Can just split by \n? 
+                        output = order.split(",") # Can just split by \n?
                         fp32_output_list.append(output[0])
 
                 layers_op[file_count].append(len(output_list))
@@ -144,17 +148,17 @@ def main():
                 mean = 0
                 median = 0
                 item = layers_op[i]
-                fp_item = fp32_layers_op[i] 
+                fp_item = fp32_layers_op[i]
                 over_flow_count = 0
                 under_flow_count = 0
-                over_flow_count += item[2].count("-64")
-                over_flow_count += item[2].count("-63.998")
-                over_flow_count += item[2].count("63.998") # Why is it 63.998 
+                over_flow_count += item[2].count("-128")
+                over_flow_count += item[2].count("-127.996")
+                over_flow_count += item[2].count("127.996") # Why is it 63.998
                 gna_values = np.array(item[2])
 
                 searchval = "-64"
                 ii = np.where(gna_values == searchval)[0]
-                ii2 = np.where(gna_values == "63.998")[0]
+                ii2 = np.where(gna_values == "128")[0]
                 fp32_overflow_vals = []
 
                 for index in ii:
@@ -178,12 +182,29 @@ def main():
 
 
             for item in layers_op:
+                print("item in layers_op " + str(item))
+                print("item[0] = " + str(item[0]))
                 output = item[0].split("_")
                 with open(result_filename, 'a', newline="\n") as csvfile:
                     layerwriter = csv.writer(csvfile, delimiter=',',
                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                    layerwriter.writerow([frame, output[0] , str(item[3]), str(item[4]), str(item[5]), str(graph.get_identity_wscale_map()[int(output[0])]), str(item[6]), str(item[7]), str(item[8]), graph.get_identity_layer_map()[int(output[0])].get_gna_name(), graph.get_identity_layer_map()[int(output[0])].get_ir_name(), graph.get_affine_layer_map()[int(output[0]) - 1].get_ir_name(), graph.get_affine_layer_map()[int(output[0]) - 1].get_wscale(), graph.get_affine_layer_map()[int(output[0]) - 1].get_oscale()])
-            #print(graph.get_identity_map().items())  
+                    print(frame)
+                    print(output[0])
+                    print(str(item[3]))
+                    print(str(item[4]))
+                    #str(item[5]),
+                    #print(str(graph.get_identity_wscale_map()[int(output[0])]))
+                    #, str(item[6]), str(item[7]), str(item[8]),
+                    #print(graph.get_identity_layer_map()[int(output[0])].get_gna_name())
+                    #, graph.get_identity_layer_map()[int(output[0])].get_ir_name(), graph.get_affine_layer_map()[int(output[0]) - 1].get_ir_name(), graph.get_affine_layer_map()[int(output[0]) - 1].get_wscale(), graph.get_affine_layer_map()[int(output[0]) - 1].get_oscale()])
+                    if (int(output[0]) != 43):
+                        if ((int(output[0]) != 205)):
+                            if( (int(output[0]) not in graph.get_identity_layer_map()) and (int(output[0]) not in graph.get_affine_layer_map())):
+                                print(output[0])
+                            else :
+                                layerwriter.writerow([frame, output[0] , str(item[3]), str(item[4]), str(item[5]), str(graph.get_identity_wscale_map()[int(output[0])]), str(item[6]), str(item[7]), str(item[8]), graph.get_identity_layer_map()[int(output[0])].get_gna_name(), graph.get_identity_layer_map()[int(output[0])].get_ir_name(), graph.get_affine_layer_map()[int(output[0]) - 1].get_ir_name(), graph.get_affine_layer_map()[int(output[0]) - 1].get_wscale(), graph.get_affine_layer_map()[int(output[0]) - 1].get_oscale()])
+
+            #print(graph.get_identity_map().items())
 
         # List of one or more "pydot.Dot" instances deserialized from this file.
 
