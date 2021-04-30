e91bfb492078c2b65b7ed5adc933625026809f32
Anisha Kulkarni <anisha.dattatraya.kulkarni@intel.com>
2021-04-30 09:04:36 -0700

    Get overflow value from gna-blob For now dont copy cpu reference data

--- check_op.py
+++ check_op.py
@@ -36,9 +36,14 @@ class LabelHTMLParser(HTMLParser):
         for item in self.list:
             if("identity" in item[5]):
                 curr_layer = gna_layers.IdentityLayer(item[5], item[2], item[29])
-                print("item2 = " + str(item[2]))
                 layer_no = item[2].split("_")
-                print("identity layer" + str(curr_layer))
+                self.identity_layer_map[int(layer_no[1])] = curr_layer
+                print("identity layer number" + str(layer_no[1]))
+                self.identity_wscale_map[int(layer_no[1])] = item[29]
+                print("identity layer wscale" + str(item[29]))
+            if("iden_infg" in item[5]):
+                curr_layer = gna_layers.IdentityLayer(item[5], item[2], item[29])
+                layer_no = item[2].split("_")
                 self.identity_layer_map[int(layer_no[1])] = curr_layer
                 print("identity layer number" + str(layer_no[1]))
                 self.identity_wscale_map[int(layer_no[1])] = item[29]
@@ -79,26 +84,85 @@ def parse_dot(dot_file_path):
     label_parser.create_layer()
     return label_parser
 
+def count_range_in_list(li, min, max):
+    ctr = 0
+    for x in li:
+        if min <= x <= max:
+            ctr += 1
+    return ctr
+
+def calc_OF_UF(curr_gna_layer_op, curr_fp32_layer_op, graph):
+    curr_gna_layer_modified = []
+    gna_values = np.array(curr_gna_layer_op[2])
+    layer_number = curr_gna_layer_op[0].split("_")
+
+    overflow_val = 32768 / float(graph.get_identity_wscale_map()[int(layer_number[0])])
+    of_count = count_range_in_list(gna_values, -overflow_val + 0.1, -overflow_val)
+    of_count += count_range_in_list(gna_values, overflow_val - 0.1, overflow_val)
+    fp32_overflow_vals = [] #-overflow_val + 1, -overflow_val, overflow_val - 1, overflow_val]
+
+    #ii = np.where((gna_values > overflow_val - 1 and gna_values < overflow_val + 0.1) or
+    #                  (gna_values < -overflow_val + 1 and gna_values > -overflow_val + 0.1))[0]
+    indexes = np.where(np.logical_and(gna_values >= (overflow_val - 1 ), gna_values <= (overflow_val + 0.1)))
+    indexes2 = np.where(np.logical_and(gna_values <= (-overflow_val), gna_values >= (-overflow_val + 0.1)))
+    for index in indexes[0]:
+        print ("indexes")
+        print((curr_gna_layer_op[2][index]))
+        fp32_overflow_vals.append(float(curr_fp32_layer_op[2][index]))
+    print("******fp of 1**********")
+    print(fp32_overflow_vals)
+    for index in indexes2[0]:
+        print ("indexes")
+        print((curr_gna_layer_op[2][index]))
+        fp32_overflow_vals.append(float(curr_fp32_layer_op[2][index]))
+    print("*******fp of*********")
+    print(fp32_overflow_vals)
+
+    #if(len(fp32_overflow_vals) != 0):
+    fp32_OF = np.array(fp32_overflow_vals)
+    fp32_OF_abs = np.abs(fp32_OF)
+    median = np.median(fp32_OF_abs)
+    mean = np.mean(fp32_OF_abs)
+
+    gna_max = max(abs(float(value)) for value in curr_gna_layer_op[2])
+    fp32_max = max(abs(float(value)) for value in curr_fp32_layer_op[2])
+    under_flow_count = curr_gna_layer_op[2].count("0")
+    curr_gna_layer_modified.append(of_count)
+    curr_gna_layer_modified.append(under_flow_count)
+    curr_gna_layer_modified.append(str(gna_max))
+    curr_gna_layer_modified.append(str(fp32_max))
+    curr_gna_layer_modified.append(str(mean))
+    curr_gna_layer_modified.append(str(median))
+
+    return curr_gna_layer_modified
+
+
+
 def main():
     if (len(sys.argv) != 7) :
-        print("Usage : python3 check_op.py no_of_frames dut_ip_address copy_layer_files=true/false parse_graph=true/false result_filename path_to_dot_file")
+        print("Usage : python3 check_op.py no_of_samples dut_ip_address copy_gna_layer_files=true/false parse_graph=true/false result_filename path_to_dot_file")
         exit()
 
-    num_frame = int(sys.argv[1])
+    num_sample = int(sys.argv[1])
     dut_ip = sys.argv[2]
     copy_files = sys.argv[3]
     parse_graph = sys.argv[4]
     result_filename = sys.argv[5]
     dot_file_path = sys.argv[6]
-    print(dut_ip)
 
     if copy_files == 'true':
-        for frame in range(0, num_frame):
-            os.mkdir(str(frame))
-            os.mkdir(str(frame) + "/identity")
-            os.mkdir(str(frame) + "/identityfp32")
-            subprocess.run(["scp", "root@" + dut_ip + ":/usr/local/ml_benchmark/layers/" + str(frame) + "/*-kActIdentity_output.txt", "./" + str(frame) + "/identity/"])
-            subprocess.run(["scp", "root@" + dut_ip + ":/usr/local/ml_benchmark/ref_layers/" + str(frame) + "/*-kActIdentity_output.txt", "./" + str(frame) + "/identityfp32"])
+        os.mkdir("gna")
+        #os.mkdir("cpu")
+        for sample in range(1, num_sample):
+            os.mkdir("gna/" + "sample" + str(sample))
+            #os.mkdir("cpu/" + "sample" + str(sample))
+            for frame in [3, 23, 37] :
+                os.mkdir("gna/" + "sample" + str(sample) + "/" + str(frame))
+                #os.mkdir("cpu/" + "sample" + str(sample) + "/" + str(frame))
+                #os.mkdir("gna/" + "sample" + str(sample) / str(frame) + "/identity")
+                #os.mkdir("sample" + str(sample) / str(frame) + "/identityfp32")
+                subprocess.run(["scp", "root@" + dut_ip + ":/usr/local/ml_benchmark/gna/sample" + str(sample) + "/" + str(frame) + "/*-kActIdentity_output.txt", "./gna/sample" + str(sample) + "/" + str(frame)])
+                #subprocess.run(["scp", "root@" + dut_ip + ":/usr/local/ml_benchmark/cpu/sample" + str(sample) + "/" + str(frame) + "/*-kActIdentity_output.txt", "./cpu/sample" + str(sample) + "/" + str(frame)])
     #convert_and_write_to_csv(0, 3, [42, 66, 54, 30, 122, 143, 186, 164, 217, 238, 259, 281, 312, 333, 354, 376, 407, 428, 449, 470, 502, 523, 544, 566], "ln_ip.csv")
     if parse_graph == 'true':
         graph = parse_dot(dot_file_path)
@@ -108,103 +172,75 @@ def main():
         with open(result_filename, 'w', newline="\n") as csvfile:
             layerwriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            layerwriter.writerow(["Frame Number ","Layer Number", "Overflow Count", "Underflow Count", "Max GNA value", "Scale Factor", "Max FP32 value", "Mean of Overflowing FP32 values", "Median", "GNA Layer Name", "IR Layer Name"])
+            layerwriter.writerow(["Sample Number", "Frame Number ","Layer Number", "Overflow Count", "Underflow Count", "Max GNA value", "Scale Factor", "Max FP32 value", "Mean of Overflowing FP32 values", "Median", "GNA Layer Name", "IR Layer Name"])
 
-        for frame in range(0, num_frame):
-            layers_op = []
-            fp32_layers_op = []
-            file_count = 0
-            for filename in os.listdir(os.getcwd() + "/" + str(frame) + "/identityfp32/"):
-                layers_op.append([])
-                fp32_layers_op.append([])
-                fp32_layers_op[file_count].append(filename)
-                layers_op[file_count].append(filename)
+        for sample in range(2, 3):
+            for frame in [3]:
+                gna_layers_op = []
+                fp32_layers_op = []
+                file_count = 0
+                for filename in os.listdir(os.getcwd() + "/" + "gna/" + "sample" + str(sample) + "/" + str(frame)):
+                    gna_layers_op.append([])
+                    fp32_layers_op.append([])
+                    gna_layers_op[file_count].append(filename)
+                    fp32_layers_op[file_count].append(filename)
 
-                with open(os.getcwd() + "/" + str(frame) + "/identity/" + str(filename), 'r') as f:
-                    layer_op = []
-                    output_list = []
+                    with open(os.getcwd() + "/" + "gna/" + "sample" + str(sample) + "/" + str(frame) + "/" + str(filename), 'r') as f:
+                        #gna_curr_layer_op = []
+                        gna_curr_output_list = []
 
-                    for data in f:
-                        order = data.replace("\n", ",").replace('\n','').strip()
-                        output = order.split(",") # Can just split by \n?
-                        output_list.append(output[0])
+                        for data in f:
+                            order = data.replace("\n", ",").replace('\n','').strip()
+                            output = order.split(",") # Can just split by \n?
+                            gna_curr_output_list.append(float(output[0])) # GNA data for current frame in gna_output_list
 
-                with open(os.getcwd() + "/" + str(frame) + "/identityfp32/" + str(filename), 'r') as f:
-                    fp32_layer_op = []
-                    fp32_output_list = []
+                    with open(os.getcwd() + "/" + "cpu/" + "sample" + str(sample) + "/" + str(frame) + "/" + str(filename), 'r') as f:
+                        #fp32_layer_op = []
+                        fp32_curr_output_list = []
 
-                    for data in f:
-                        order = data.replace("\n", ",").replace('\n','').strip()
-                        output = order.split(",") # Can just split by \n?
-                        fp32_output_list.append(output[0])
+                        for data in f:
+                            order = data.replace("\n", ",").replace('\n','').strip()
+                            output = order.split(",") # Can just split by \n?
+                            fp32_curr_output_list.append(float(output[0])) # CPU data for current frame in gna_output_list
 
-                layers_op[file_count].append(len(output_list))
-                layers_op[file_count].append(output_list)
-                fp32_layers_op[file_count].append(len(fp32_output_list))
-                fp32_layers_op[file_count].append(fp32_output_list)
-                file_count += 1
+                    gna_layers_op[file_count].append(len(gna_curr_output_list))
+                    gna_layers_op[file_count].append(gna_curr_output_list)
+                    fp32_layers_op[file_count].append(len(fp32_curr_output_list))
+                    fp32_layers_op[file_count].append(fp32_curr_output_list)
+                    file_count += 1
+                    assert(len(gna_curr_output_list) == len(fp32_curr_output_list))
 
-            for i in range(0, len(layers_op)):
-                mean = 0
-                median = 0
-                item = layers_op[i]
-                fp_item = fp32_layers_op[i]
-                over_flow_count = 0
-                under_flow_count = 0
-                over_flow_count += item[2].count("-128")
-                over_flow_count += item[2].count("-127.996")
-                over_flow_count += item[2].count("127.996") # Why is it 63.998
-                gna_values = np.array(item[2])
+                modified_gna_layers_op = []
+                for i in range(0, len(gna_layers_op)):
+                    curr_gna_layer_op = gna_layers_op[i]
+                    curr_cpu_layer_op = fp32_layers_op[i]
+                    curr_gna_layer_modified = []
+                    curr_gna_layer_modified.append(curr_gna_layer_op[0])
+                    curr_gna_layer_modified.append(curr_gna_layer_op[1])
+                    curr_gna_layer_modified.append(curr_gna_layer_op[2])
+                    modified_layer_data = calc_OF_UF(curr_gna_layer_op, curr_cpu_layer_op, graph)
+                    curr_gna_layer_modified.extend(modified_layer_data)
+                    modified_gna_layers_op.append(curr_gna_layer_modified)
 
-                searchval = "-64"
-                ii = np.where(gna_values == searchval)[0]
-                ii2 = np.where(gna_values == "128")[0]
-                fp32_overflow_vals = []
+                for curr_gna_layer in modified_gna_layers_op:
+                    layer_number_array = curr_gna_layer[0].split("_")
+                    layer_num = int(layer_number_array[0])
+                    with open(result_filename, 'a', newline="\n") as csvfile:
+                        layerwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
-                for index in ii:
-                    fp32_overflow_vals.append(float(fp_item[2][index]))
-                for index in ii2:
-                    fp32_overflow_vals.append(float(fp_item[2][index]))
-                if(len(fp32_overflow_vals) != 0):
-                    fp32_OF = np.array(fp32_overflow_vals)
-                    fp32_OF_abs = np.abs(fp32_OF)
-                    median = np.median(fp32_OF_abs)
-                    mean = np.mean(fp32_OF_abs)
-                gna_max = max(abs(float(value)) for value in item[2])
-                fp32_max = max(abs(float(value)) for value in fp_item[2])
-                under_flow_count += item[2].count("0")
-                item.append(over_flow_count)
-                item.append(under_flow_count)
-                item.append(str(gna_max))
-                item.append(str(fp32_max))
-                item.append(str(mean))
-                item.append(str(median))
-
-
-            for item in layers_op:
-                print("item in layers_op " + str(item))
-                print("item[0] = " + str(item[0]))
-                output = item[0].split("_")
-                with open(result_filename, 'a', newline="\n") as csvfile:
-                    layerwriter = csv.writer(csvfile, delimiter=',',
-                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                    print(frame)
-                    print(output[0])
-                    print(str(item[3]))
-                    print(str(item[4]))
-                    #str(item[5]),
-                    #print(str(graph.get_identity_wscale_map()[int(output[0])]))
-                    #, str(item[6]), str(item[7]), str(item[8]),
-                    #print(graph.get_identity_layer_map()[int(output[0])].get_gna_name())
-                    #, graph.get_identity_layer_map()[int(output[0])].get_ir_name(), graph.get_affine_layer_map()[int(output[0]) - 1].get_ir_name(), graph.get_affine_layer_map()[int(output[0]) - 1].get_wscale(), graph.get_affine_layer_map()[int(output[0]) - 1].get_oscale()])
-                    if (int(output[0]) != 43):
-                        if ((int(output[0]) != 205)):
-                            if( (int(output[0]) not in graph.get_identity_layer_map()) and (int(output[0]) not in graph.get_affine_layer_map())):
-                                print(output[0])
-                            else :
-                                layerwriter.writerow([frame, output[0] , str(item[3]), str(item[4]), str(item[5]), str(graph.get_identity_wscale_map()[int(output[0])]), str(item[6]), str(item[7]), str(item[8]), graph.get_identity_layer_map()[int(output[0])].get_gna_name(), graph.get_identity_layer_map()[int(output[0])].get_ir_name(), graph.get_affine_layer_map()[int(output[0]) - 1].get_ir_name(), graph.get_affine_layer_map()[int(output[0]) - 1].get_wscale(), graph.get_affine_layer_map()[int(output[0]) - 1].get_oscale()])
-
-            #print(graph.get_identity_map().items())
+                        if( (int(layer_num) not in graph.get_identity_layer_map()) and (int(layer_num) not in graph.get_affine_layer_map())):
+                            print("****item missing")
+                            print(layer_num)
+                        else :
+                            layerwriter.writerow([sample, frame, layer_num, str(curr_gna_layer[3]), str(curr_gna_layer[4]),
+                                                    str(curr_gna_layer[5]), str(graph.get_identity_wscale_map()[layer_num]),
+                                                    str(curr_gna_layer[6]), str(curr_gna_layer[7]), str(curr_gna_layer[8]),
+                                                    graph.get_identity_layer_map()[layer_num].get_gna_name(),
+                                                    graph.get_identity_layer_map()[layer_num].get_ir_name(),
+                                                    graph.get_affine_layer_map()[layer_num - 1].get_ir_name(),
+                                                    graph.get_affine_layer_map()[layer_num - 1].get_wscale(),
+                                                    graph.get_affine_layer_map()[layer_num - 1].get_oscale()])
 
         # List of one or more "pydot.Dot" instances deserialized from this file.
 
